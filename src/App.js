cd45d75bb9cf719f59d636a093a684a3f1c22d2f
estimar2 <songsm5504@gmail.com>
2024-01-25 17:01:59 +0900

    [24.01.25] Part1-4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  // 자바스크립트 destructuring 문법 : 내가 array 안에 있는 데이터들을 변수로 쉽게 저장하고 싶으면 쓰는 문법
+  // + state 는 변동사항이 생기면 state쓰는 html 도 지동으로 재랜더링 해줌
   const [listData, setListData] = useState([
     { title: "남자코드 추천", date: "2월 17일 발행" },
     { title: "강남 우동맛집", date: "2월 17일 발행" },
