2e9a72592912af747356d49a4bfba44d94a73c43
estimar2 <songsm5504@gmail.com>
2024-01-25 17:00:49 +0900

    [24.01.25] Part1-4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  let post = "강남 우동 맛집";
+  const [listData, setListData] = useState([
+    { title: "남자코드 추천", date: "2월 17일 발행" },
+    { title: "강남 우동맛집", date: "2월 17일 발행" },
+    { title: "파이썬 독학", date: "2월 17일 발행" },
+  ]);
 
   return (
     <div className="App">
@@ -9,7 +14,12 @@ function App() {
         <h4>블로그임</h4>
       </div>
 
-      <h4>{post}</h4>
+      {listData.map(({ title, date }) => (
+        <div className="list">
+          <h4>{title}</h4>
+          <p>{date}</p>
+        </div>
+      ))}
     </div>
   );
 }
