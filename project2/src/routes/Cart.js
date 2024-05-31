0097ec3b07fe327928d397aeac91194da70f9bd9
estimar2 <songsm5504@gmail.com>
2024-05-31 17:50:20 +0900

    [24.05.31] Part2-17 : Redux2 : store에 state보관하고 쓰는법

--- project2/src/routes/Cart.js
+++ project2/src/routes/Cart.js
@@ -1,34 +1,39 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Table } from "react-bootstrap";
 
 const Cart = () => {
   // Redux를 사용하면 컴포넌트들이 props없이 state공유 가능(리액트 구인시 대부분 Redux요구)
-  // 1. store.js 파일 생성
-  // 2. index.js파일가서 <Provider store={store}>쓰기
+  // 셋팅
+  //    1. store.js 파일 생성
+  //    2. index.js파일가서 <Provider store={store}>쓰기
+  // Redux store의 state꺼내쓰는법 : useSelector(() => {})
+  // Redux store안에 모든걸 넣지말고, 컴포넌트간 공유가 필요없으면 그냥 useState() 쓰면됨
+  let state = useSelector(state => state.cartList);
+  console.log(state, ">> state");
+
   return (
     <>
-      <Table striped bordered hover>
+      <Table bordered hover>
         <thead>
           <tr>
             <th>#</th>
             <th>상품명</th>
             <th>수량</th>
-            <th>변경하기</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
+          {state.length !== 0
+            ? state.map((data, idx) => (
+                <>
+                  <tr key={idx}>
+                    <td>{data.id}</td>
+                    <td>{data.name}</td>
+                    <td>{data.count}</td>
+                  </tr>
+                </>
+              ))
+            : null}
         </tbody>
       </Table>
     </>
--- project2/src/store.js
+++ project2/src/store.js
@@ -1,5 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+// Redux 왜씀?
+// - component간 state공유 편해짐
+
+// Redux store에 state보관하는 법
+// createSlice()로 state(state하나를 slice라고 부름)만들고 = useState()역할임
+//               name : state이름, initialState : state값
+// configureStore reducer에 {작명 : createSlice만든거.reducer}형태로 등록해야 사용 가능
+
+let user = createSlice({
+  name: "user",
+  initialState: "kim",
+});
+
+let cartList = createSlice({
+  name: "cartList",
+  initialState: [
+    { id: 0, name: "White and Black", count: 2 },
+    { id: 2, name: "Grey Yordan", count: 1 },
+  ],
+});
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    user: user.reducer,
+    cartList: cartList.reducer,
+  },
 });
